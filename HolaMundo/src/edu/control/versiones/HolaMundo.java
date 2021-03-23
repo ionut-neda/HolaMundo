3933699096b13d9b52a8b368912c9a4698f602ad
ionut-neda2 <81244992+ionut-neda2@users.noreply.github.com>
2021-03-23 15:24:00 +0100

    Modificación por ionut-neda2
    Esto es una modificación

--- HolaMundo/src/edu/control/versiones/HolaMundo.java
+++ HolaMundo/src/edu/control/versiones/HolaMundo.java
@@ -9,7 +9,7 @@ public class HolaMundo {
 		//Esto es un comentario para comprobar el uso de ramas
 		System.out.println("Hola Mundo desde repositorio remoto");
 		System.out.println("Nueva línea de código escrita por el usuario alvaroramas");
-		
+		System.out.println("Hola Mundo por ionut-neda2");
 		
 	}
 
